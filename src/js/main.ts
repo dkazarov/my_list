1be9863b87cf06f8b32494cd3e5b2e2d09b46675
unknown <d.kazarov1361@gmail.com>
2025-08-05 20:49:25 +0300

    js: add func hide clear btn after click to document

--- src/js/main.ts
+++ src/js/main.ts
@@ -8,10 +8,35 @@ const inputField = document.querySelector('.user__input-field') as HTMLInputElem
 const inputClear = document.querySelector('.user__input-ferld--clear') as HTMLButtonElement | null;
 const inputBtn = document.querySelector('.user__input-btn') as HTMLButtonElement | null;
 
-inputField?.addEventListener('input', () => {
-	if (inputField && inputField.value !== '') {
-		inputClear?.classList.remove('--hide');
-	} else {
-		inputClear?.classList.add('--hide');
+// Ініціалізація кнопки очистки інпуту
+const writeInput = () => {
+	inputField?.addEventListener('input', () => {
+		if (inputField && inputField.value !== '') {
+			inputClear?.classList.remove('--hide');
+		} else {
+			inputClear?.classList.add('--hide');
+		}
+	});
+};
+
+// Ховає хрестик якщо клацнув поза інпутом
+const hideInputClear = () => {
+	document.addEventListener('click', e => {
+		e.target as HTMLElement;
+		if (!inputField?.contains(e.target as Node) && !inputClear?.contains(e.target as Node)) {
+			inputClear?.classList.add('--hide');
+			clearInputValue();
+		}
+	});
+};
+
+// Очистка інпуту
+const clearInputValue = () => {
+	if (inputField) {
+		inputField.value = '';
 	}
-});
+};
+
+// Attach hideInputClear as a click event listener to the document
+hideInputClear();
+writeInput();
