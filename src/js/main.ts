14a2a9d93e06d53d6cb4034e1376ff0a1beda69a
unknown <d.kazarov1361@gmail.com>
2025-08-05 20:20:38 +0300

    js: add input remove and add class hide after input and clear text

--- src/js/main.ts
+++ src/js/main.ts
@@ -3,3 +3,15 @@ import { searchInit } from './js-components/search';
 
 searchInit();
 initBurger();
+
+const inputField = document.querySelector('.user__input-field') as HTMLInputElement | null;
+const inputClear = document.querySelector('.user__input-ferld--clear') as HTMLButtonElement | null;
+const inputBtn = document.querySelector('.user__input-btn') as HTMLButtonElement | null;
+
+inputField?.addEventListener('input', () => {
+	if (inputField && inputField.value !== '') {
+		inputClear?.classList.remove('--hide');
+	} else {
+		inputClear?.classList.add('--hide');
+	}
+});
