f96a9a55e9d2188b1ce0e3230ef387688371f095
Dmytro Kazarov <d.kazarov1361@gmail.com>
2025-10-04 12:19:05 +0300

    fix unusable event to DomContentLoaded

--- src/js/main.ts
+++ src/js/main.ts
@@ -3,56 +3,56 @@ import { searchInit } from './js-components/search';
 import { userInputInstall } from './js-components/input';
 import { getAndRenderItems } from './js-components/API/getAllItems';
 
-document.addEventListener('DOMContentLoaded', e => {
+document.addEventListener('DOMContentLoaded', () => {
 	searchInit();
 	initBurger();
 	userInputInstall();
 	getAndRenderItems();
 });
 
-	const HOST_API: string = 'http://localhost:8888/my_list/api.php';
+const HOST_API: string = 'http://localhost:8888/my_list/api.php';
 
-	const addBtn = document.querySelector('.user__btn-add') as HTMLButtonElement;
-	const userInput = document.querySelector('.user__input') as HTMLInputElement;
+const addBtn = document.querySelector('.user__btn-add') as HTMLButtonElement;
+const userInput = document.querySelector('.user__input') as HTMLInputElement;
 
-	interface NewItem {
-		title: string;
-		completed: boolean;
+interface NewItem {
+	title: string;
+	completed: boolean;
+}
+
+addBtn.addEventListener('click', event => {
+	event.preventDefault();
+
+	const userData = userInput.value.trim();
+
+	if (userData !== '' && userData !== ' ') {
+		postData();
+	} else {
+		alert('Поле не може бути пустим!!!');
 	}
+});
 
-	addBtn.addEventListener('click', e => {
-		e.preventDefault();
+const postData = async () => {
+	try {
+		const response = await fetch(HOST_API, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				title: userInput.value,
+				completed: true,
+			} as NewItem),
+		});
 
-		const userData = userInput.value.trim();
-
-		if (userData !== '' && userData !== ' ') {
-			postData();
-		} else {
-			alert('Поле не може бути пустим!!!');
+		if (!response.ok) {
+			throw new Error(`HTTP ошибка: ${response.status}`);
 		}
-	});
 
-	const postData = async () => {
-		try {
-			const response = await fetch(HOST_API, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					title: userInput.value,
-					completed: true,
-				} as NewItem),
-			});
-
-			if (!response.ok) {
-				throw new Error(`HTTP ошибка: ${response.status}`);
-			}
-
-			const data = await response.json();
-			return data;
-		} catch (error) {
-			alert('Не вдалося выдправити дані');
-			return null;
-		}
-	};
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		alert('Не вдалося выдправити дані');
+		return null;
+	}
+};
